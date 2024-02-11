1117d72ba8c75a391aa8006905fd70e7a7a5b57e
Vitalii-Chaplygin <chapliginvitaliy@gmail.com>
2024-02-12 00:12:51 +0100

    add Lesson16

---
+++ src/homeWork_19/Task3Sort.java
@@ -0,0 +1,34 @@
+package homeWork_19;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class Task3Sort {
+    public static void sotrarray(int[] array) {
+        for (int i = 0; i < array.length - 1; i++) {
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[i] < array[j]) {
+                    int result = array[i];
+                    array[i]= array[j];
+                    array[j] = result;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int length = 8;
+        int[] intArr = new int[length];
+        Random random = new Random();
+        for (int i = 0; i < length; i++) {
+            intArr[i] = random.nextInt(10) + 1;
+
+
+        }
+        sotrarray(intArr);
+        System.out.println(Arrays.toString(intArr));
+    }
+
+}
+
+
